f05f717dc023f5fb0d028d3ad1c3f4b527abb2e9
Scott Allen <scott-allen@lambdastudents.com>
2020-10-08 14:56:17 -0600

    Added styling using styled components

--- src/Image/Image.js
+++ src/Image/Image.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const StyledNasaImage = styled.div`
+    img {
+        width:50%;
+
+        @media (max-width: 800px) {
+            width:100%;
+        }
+    }
+`
 function Image(props) {
     const { source } = props
 
     return (
-        <div className = 'nasaImage'>
+        <StyledNasaImage>
             <img alt='picture of the day from Nasa' src={source}/>
-        </div>
+        </StyledNasaImage>
     )
 }
 
--- src/Info/Info.js
+++ src/Info/Info.js
@@ -1,14 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledNasaInfo = styled.div`
+    span {
+        margin: .5%;
+        font-weight: bold;
+    }
+    
+    p{
+        width:80%;
+        margin: auto;
+    }
+`
 
 function Info (props){
     const { copyright, date, explanation } = props
 
     return (
-        <div>
+        <StyledNasaInfo>
             <span>{copyright}</span>
             <span>{date}</span>
             <p>{explanation}</p>
-        </div>
+        </StyledNasaInfo>
     );
 };
 
